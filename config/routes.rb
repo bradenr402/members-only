0e3989bde98de56d78186f3d9cacaf0aaa0183e7
Braden <108199884+bradenr402@users.noreply.github.com>
2023-10-09 11:51:05 -0400

    Project complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    get 'sign_out', to: 'devise/sessions#destroy', as: 'sign_out_path'
+    get 'sign_in', to: 'devise/sessions#new', as: 'sign_in_path'
+  end
+
+  root "posts#index"
+  resources :posts, only: [:new, :create, :index]
 end
